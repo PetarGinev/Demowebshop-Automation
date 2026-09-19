4be83eb5a0f980787b1cd1d9d1cfa23362f76cc3
Petar Ginev <pginev7@gmail.com>
2026-09-19 10:12:03 +0300

    Remove .only for TC55

--- test/product.spec.ts
+++ test/product.spec.ts
@@ -33,7 +33,7 @@ test.describe('Category & Product Pages test suite', () => {
     await TC41(page);
   });
 
-  test.only('TC55 - Breadcrumb link on a product page navigates back to its category', async ({ page }) => {
+  test('TC55 - Breadcrumb link on a product page navigates back to its category', async ({ page }) => {
     await TC55(page);
   });
 
